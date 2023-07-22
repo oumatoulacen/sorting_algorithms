a944b49006c9abfc5f2beca6ab8df4ee69425754
oumatoulacen <oumatoulacen@gmail.com>
2023-07-22 11:58:49 +0100

    insertion_sort_list

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -10,7 +10,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *curr;
 
-	if (!(list || *list))
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
 	curr = (*list)->next;
