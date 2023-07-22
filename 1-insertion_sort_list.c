7573486bd6375c216f10f46d965365af11abdc26
oumatoulacen <oumatoulacen@gmail.com>
2023-07-22 11:49:26 +0100

    insertion_sort_list

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,32 +1,6 @@
 #include "sort.h"
 
 /**
- * swap_nodes - swap two nodes of a double linked list
- * @f_node: first node
- * @s_node: second node
- * @node: the head of the list
- */
-
-void swap_nodes(listint_t **node, listint_t **f_node, listint_t **s_node)
-{
-	listint_t *f_prev = (*f_node)->prev;
-	listint_t *s_next = (*s_node)->next;
-
-	(*f_node)->next = s_next;
-	if ((*s_node)->next)
-		(*s_node)->next->prev = *f_node;
-	(*f_node)->prev = *s_node;
-	(*s_node)->prev = f_prev;
-	(*s_node)->next = *f_node;
-
-	if ((*f_node)->prev)
-		(*f_node)->prev->next = (*s_node);
-	else
-		*node = (*s_node);
-}
-
-
-/**
  * insertion_sort_list - sorts a doubly linked list of integers in ascending
  * order using the Insertion sort algorithm
  * @list: doubly linked list to sort
@@ -44,7 +18,16 @@ void insertion_sort_list(listint_t **list)
 	{
 		while (curr->prev && curr->prev->n > curr->n)
 		{
-			swap_nodes(list, &curr->prev, &curr);
+			curr->prev->next = curr->next;
+			if (curr->next)
+				curr->next->prev = curr->prev;
+			curr->next = curr->prev;
+			curr->prev  = curr->next->prev;
+			curr->next->prev = curr;
+			if (curr->prev)
+				curr->prev->next = curr;
+			else
+				*list = curr;
 			print_list((const listint_t *)*list);
 		}
 		curr = curr->next;
