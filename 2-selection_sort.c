08f05131caf19db3cb776476809da2d63f41f303
oumatoulacen <oumatoulacen@gmail.com>
2023-07-22 12:33:22 +0100

    selection_sort_array

---
+++ 2-selection_sort.c
@@ -0,0 +1,47 @@
+#include "sort.h"
+
+/**
+ * swap - swaps two elements
+ * @ele1: first element
+ * @ele2: second element
+ */
+void swap(int *ele1, int *ele2)
+{
+	int tmp;
+
+	tmp = *ele1;
+	*ele1 = *ele2;
+	*ele2 = tmp;
+}
+
+/**
+ * selection_sort - sorts an array of integers in ascending order using
+ * the Selection sort algorithm
+ * @array: array to sort
+ * @size: size of the array
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	int key;
+	int i, j, index, swapped = 0;
+
+	for (i = 0; i < (int)size - 1; i++)
+	{
+		key = array[i];
+		for (j = i + 1; j < (int)size; j++)
+		{
+			if (key > array[j])
+			{
+				key = array[j];
+				index = j;
+				swapped = 1;
+			}
+		}
+		if (swapped)
+		{
+			swap(&array[i], &array[index]);
+			print_array(array, size);
+		}
+	}
+}
